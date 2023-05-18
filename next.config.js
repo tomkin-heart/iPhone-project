7fa432e8a05d977fcc37ca91fcffbe647f9b310d
tomkin-heart <tomkin-heart@yandex.ru>
2023-05-18 22:57:49 +0200

    build process fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/* module.exports = {
 	experimental: {appDir: true},
 	output: 'export',
 	distDir: 'build',
 	trailingSlash: true,
+} */
+
+module.exports = {
+	experimental: {appDir: true},
+	output: 'export',
+	assetPrefix:
+		process.env.NODE_ENV === 'production' ? '/iPhone-project/' : '',
+	images: {unoptimized: true},
 }
